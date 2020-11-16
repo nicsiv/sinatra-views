ec0dcf31d933ebf89eba9ff1852cb57bbbbcb05a
nicsiv <nicoleusivek@gmail.com>
2020-11-16 16:41:40 -0600

    commit tree 614436aede25d6db2abfcee59a3564d9ce445623

--- app.rb
+++ app.rb
@@ -3,6 +3,24 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		"<h1>Hello World</h1>"	
 	end
+
+	get "/info" do
+		"Testing the info page"
+	end
+
+	get '/' do
+		erb :index
+	end
+
+	get '/' do 
+		erb :info
+	end
+
+	get "/info" do
+		erb :dogs
+	  end
+
+
 end
